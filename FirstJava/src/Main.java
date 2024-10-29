bd80774c4c5ab1ff5a23505a3b2fdb42b4f75ccb
loveAlakazam <dmsrkd1216@gmail.com>
2024-10-29 22:52:19 +0900

    :sparkles: Init: FirstJava 프로젝트 생성

---
+++ FirstJava/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    // [JDK]
+    // (1) compiler: .java -> .class
+    // (2) JRE
+    // (3) JDK: 디버깅
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
