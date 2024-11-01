c3091475df8f2f0ccf9840557bb81efdd27a0761
eunkang-dyphi <eunkang@dyphi.com>
2024-11-01 09:13:07 +0900

    :sparkles: Add: 자동형변환샘플코드작성

---
+++ FirstJava/src/week01/selfReview/typecastings/TypeCastingPractice01.java
@@ -0,0 +1,28 @@
+package week01.selfReview.typecastings;
+
+public class TypeCastingPractice01 {
+    public static void main( String[] args ) {
+        // 자동 캐스팅 1. short -> int
+        short shortDish = 10; // 작은그릇
+        int intDish; // 큰그릇
+
+        intDish = shortDish; // 작은그릇에 담은 데이터를 큰그릇에 옮김.
+        System.out.println( "short -> int: " + intDish );
+
+        // 자동 캐스팅 2.int -> long
+        long longDish;
+        intDish = 2147483647;
+        longDish = intDish;
+        System.out.println( "int -> long: " + longDish );
+
+        // 자동 캐스팅 3. long -> float
+        longDish = 21474836489L;
+        float floatDish = longDish;
+        System.out.println( "long -> float: " + floatDish );
+
+        // 자동 캐스팅 4. float -> double
+        floatDish = 3.1415921753f;
+        double doubleDish = floatDish;
+        System.out.println( "float -> double: " + doubleDish );
+    }
+}
